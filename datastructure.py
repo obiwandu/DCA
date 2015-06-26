f6fa9e6566107e8f082329878f9c6b1c2fabf83a
obiwandu <obiwandu@gmail.com>
2015-06-26 17:11:09 +0800

    Extract the data structure of command, execution info, etc. So that they would be more easily expanded in the future.

---
+++ datastructure.py
@@ -0,0 +1,17 @@
+__author__ = 'User'
+
+class Identity:
+    def __init__(self):
+        self.ip = None
+        self.dev_id = None
+        self.dev_pw = None
+        return
+
+class Command:
+    def __init__(self):
+        self.abs_cmd = None
+        self.act_cmd = None
+        self.factory = None
+        self.model = None
+        self.exp_res = []
+        return
